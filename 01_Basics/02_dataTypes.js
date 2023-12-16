9b4ced3330f5345905353e0fbf9713dd755af316
santoshobra <santoshobra@gmail.com>
2023-12-16 18:18:56 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") // we are using nodejs, not browser
+
+let name = "Santosh"
+let age = 18
+let isLoggedIn = false
+
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
