bd52a74267f4ba2ae915f37e712ee093e32f50cf
santoshobra <santoshobra@gmail.com>
2023-12-17 06:05:38 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = undefined; //null; //"33av"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1
+
+let isLoggedIn = "abc"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
